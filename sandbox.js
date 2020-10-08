e70565c2e27f7193e59fe7a86e3cf9b4b903922e
DavidPokrajac <d0pokrajac@gmail.com>
2020-10-08 11:07:48 +0200

    Added event listeners for lower count button and add count button

---
+++ sandbox.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+
+  // Referencing HTML tags
+  var number = document.querySelector("#number");
+
+  var lowerCount = document.getElementById("lower-count");
+
+  var addCount = document.getElementById("add-count");
+
+  var arr = 0;
+  number.textContent = arr;
+
+  // Adding event listener when lower count button is clicked
+  lowerCount.addEventListener("click", function(){
+
+    
+
+  });
+
+  // Adding event listener when add coutn button is clicked
+  addCount.addEventListener("click", function(){
+
+
+
+  });
+
+}
