e7075869b0fbee14d559a1968437d0d50d3a72e8
DavidPokrajac <d0pokrajac@gmail.com>
2020-10-08 11:24:24 +0200

    Added logic for both event listeners, together with if and else if statements to check if textContent value is less than, more than  or equal to 0, and then applying certain styles to it

--- sandbox.js
+++ sandbox.js
@@ -13,14 +13,32 @@ window.onload = function(){
   // Adding event listener when lower count button is clicked
   lowerCount.addEventListener("click", function(){
 
-    
+    //When clicked, arr variable will decrement by 1 and we'll set the textContent of the number equale to that array value
+    arr--;
+    number.textContent = arr;
+
+    // If less than 0, the color of the text will be red; if equal to 0, the text will remain black
+    if(arr < 0){
+      number.style.color = "firebrick";
+    } else if(arr === 0){
+      number.style.color = "black";
+    }
 
   });
 
   // Adding event listener when add coutn button is clicked
   addCount.addEventListener("click", function(){
 
+    // When clicked, arr variable will increment by 1 amd we'll again set the textContent of number to array value
+    arr++;
+    number.textContent = arr;
 
+    // If more than 0; the color of the text will be green; if equal to 0, the text will remain black
+    if(arr > 0){
+      number.style.color = "seagreen";
+    }else if(arr === 0){
+      number.style.color = "black";
+    }
 
   });
 
